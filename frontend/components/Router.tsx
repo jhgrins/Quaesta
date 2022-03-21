ee868a5331f1706153f1e4c2a568be11fd5d8144
Max Rosoff <me@maxrosoff.com>
2022-03-20 20:18:41 -0700

    Update Routes For Pages SPA

--- frontend/components/Router.tsx
+++ frontend/components/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, useNavigate, useLocation } from "react-router-dom";
 
 import LoginLayout from "./Login/Layout";
 import CreateAccount from "./Login/CreateAccount";
@@ -12,29 +12,46 @@ import DashBoard from "./App/Pages/Dashboard";
 import Game from "./App/Game/Game";
 import Profile from "./App/Pages/Profile";
 import NotFound from "./NotFound";
+import { useEffect } from "react";
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path={"/"} element={<LoginLayout />}>
-					<Route index element={<Login />} />
-					<Route path={"create-account"} element={<CreateAccount />} />
-					<Route path={"login"} element={<Login />} />
-					<Route path={"finalize-account"} element={<FinalizeAccount />} />
-					<Route path={"forgot-password"} element={<ForgotPassword />} />
-					<Route path={"forgot-password/:authToken"} element={<ForgotPassword />} />
+				<Route path={"/"} element={<Router404Inject />}>
+					<Route element={<LoginLayout />}>
+						<Route index element={<Login />} />
+						<Route path={"create-account"} element={<CreateAccount />} />
+						<Route path={"login"} element={<Login />} />
+						<Route path={"finalize-account"} element={<FinalizeAccount />} />
+						<Route path={"forgot-password"} element={<ForgotPassword />} />
+						<Route path={"forgot-password/:authToken"} element={<ForgotPassword />} />
+					</Route>
+					<Route path={"/app"} element={<AppLayout />}>
+						<Route index element={<DashBoard />} />
+						<Route path={"profile"} element={<Profile />} />
+						<Route path={"dashboard"} element={<DashBoard />} />
+						<Route path={"game/:gameName"} element={<Game />} />
+					</Route>
+					<Route path={"*"} element={<NotFound />} />
 				</Route>
-				<Route path={"/app"} element={<AppLayout />}>
-					<Route index element={<DashBoard />} />
-					<Route path={"profile"} element={<Profile />} />
-					<Route path={"dashboard"} element={<DashBoard />} />
-					<Route path={"game/:gameName"} element={<Game />} />
-				</Route>
-				<Route path={"*"} element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
+};
+
+const Router404Inject = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		const splitPath = location.search.split("/");
+		if (splitPath[0] === "?") {
+			navigate(splitPath[1]);
+		}
+	}, [location]);
+
+	return <Outlet />;
 };
 
 export default Router;
